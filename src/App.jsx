c033b7e9099c06454ab6b56fb748a2071ceb011a
Om Mahajna <omrmahajan9477@gmail.com>
2025-09-13 01:10:25 +0530

    first commit and make a basic vite react file having tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+
+  return (
+    <>
+      
+      <div>
+
+         <p className="text-6xl text-gray-700">This is main folder</p>
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
